77b4f0d181eb1f9edf21b4ef45f654689033d1f2
samedberry <143440194+samedberry@users.noreply.github.com>
2023-12-10 10:09:47 -0700

    update 1
    created classes
    finished Die

---
+++ src/Die.java
@@ -0,0 +1,44 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Die {
+    private int numSides;
+    private int value;
+
+    public Die () {
+        this.numSides = 6;
+        this.value = 1;
+    }
+
+    public Die (int numSides) {
+        this.numSides = numSides;
+        this.value = 1;
+    }
+
+    public void roll() {
+        this.value = ThreadLocalRandom.current().nextInt(1, this.numSides+1);
+    }
+
+    public int getNumSides() {
+        return numSides;
+    }
+
+    public void setNumSides(int numSides) {
+        this.numSides = numSides;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Die{" +
+                "numSides=" + numSides +
+                ", value=" + value +
+                '}';
+    }
+}
